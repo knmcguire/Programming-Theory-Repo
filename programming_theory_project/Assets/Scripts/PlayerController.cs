648d3c60779e24fa9aeaf9481a3ff338ad1d9a22
knmcguire <k.n.mcguire@tudelft.nl>
2021-09-03 15:35:26 +0200

    added prototype player with controlling script

---
+++ programming_theory_project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float rangeArena = 5;
+    private float speed = 10.0f;
+    private float turnSpeed = 90.0f;
+
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeepPlayerInArena();
+        ControlPlayer();
+
+    }
+
+    // Keyboard input to control the player
+    void ControlPlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+
+// Check if player reached the end of the arena
+    void KeepPlayerInArena()
+    {
+        if (transform.position.x < -rangeArena)
+        {
+            transform.position = new Vector3(-rangeArena, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > rangeArena)
+        {
+            transform.position = new Vector3(rangeArena, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < -rangeArena)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -rangeArena);
+        }
+        if (transform.position.z > rangeArena)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, rangeArena);
+        }
+    }
+    
+}
