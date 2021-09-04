5244f12bc65cee49938d6f89368858b5b87308a3
knmcguire <k.n.mcguire@tudelft.nl>
2021-09-04 12:18:58 +0200

    make a battery prefab and include it in spawn manager

---
+++ programming_theory_project/Assets/Scripts/Battery.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+
+    private float turnSpeed = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector3.up);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.up, turnSpeed * Time.deltaTime);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        { 
+            Destroy(gameObject);
+        }
+    }
+}
